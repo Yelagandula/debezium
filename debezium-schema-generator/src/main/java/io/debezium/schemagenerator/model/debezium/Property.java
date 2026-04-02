9671776162a350b4c3cfb8bdad0fd767f06b192d
Vineeth Yelagandula <111960524+Yelagandula@users.noreply.github.com>
2026-04-02 02:59:30 -0500

    Add defaultValue property to Property record
    Signed-off-by: Vineeth Yelagandula <111960524+Yelagandula@users.noreply.github.com>

--- debezium-schema-generator/src/main/java/io/debezium/schemagenerator/model/debezium/Property.java
+++ debezium-schema-generator/src/main/java/io/debezium/schemagenerator/model/debezium/Property.java
@@ -15,11 +15,12 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
  * Configuration property.
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "name", "type", "required", "display", "validation", "valueDependants" })
+@JsonPropertyOrder({ "name", "type", "required", "default", "display", "validation", "valueDependants" })
 public record Property(
         @JsonProperty("name") String name,
         @JsonProperty("type") String type,
         @JsonProperty("required") Boolean required,
+        @JsonProperty("default") String defaultValue,
         @JsonProperty("display") Display display,
         @JsonProperty("validation") List<Validation> validation,
         @JsonProperty("valueDependants") List<ValueDependant> valueDependants) {
